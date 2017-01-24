6c807461baa40fd89cff36f80a03fa3c72e6c24f
Monicadorthy <monicadorthy@gmail.com>
2017-01-24 14:22:19 +0530

    Sixth GitHub Commit
    Sixth GitHub Commit

---
+++ app4Assignment6.4.js
@@ -0,0 +1,9 @@
+var myarray=[{EmployeeName:"Ram",age:24,salary:15000, city:"Bangalore",state:"Karnataka",pincode:560026},
+{EmployeeName:"Priya",age:25,salary:15500, city:"Bangalore",state:"Karnataka",pincode:560028},
+{EmployeeName:"Surya",age:24,salary:18000, city:"Bangalore",state:"Karnataka",pincode:560026},
+{EmployeeName:"Monica",age:24,salary:20000, city:"Bangalore",state:"Karnataka",pincode:560027},
+{EmployeeName:"Sooraj",age:26,salary:30000, city:"Bangalore",state:"Karnataka",pincode:560029}
+];
+for(item in myarray){
+    console.log(myarray[item]);
+}
